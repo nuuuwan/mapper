695b9fa18325aa628eefaed0dc9102c9cddc9d8d
nuuuwan <nuuuwan@gmail.com>
2023-11-10 13:10:01 +0530

    lint only

--- src/view/pages/pages.js
+++ src/view/pages/pages.js
@@ -1,7 +1,7 @@
 import PublicIcon from "@mui/icons-material/Public";
 
 import ListIcon from "@mui/icons-material/List";
-import EditNoteIcon from '@mui/icons-material/EditNote';
+import EditNoteIcon from "@mui/icons-material/EditNote";
 export const PAGE_IDX = {
   config: { Icon: ListIcon },
   map: { Icon: PublicIcon },
