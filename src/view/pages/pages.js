ff37a4a117089fc1d42b2ad7b1d81c0c8ab5ea5c
nuuuwan <nuuuwan@gmail.com>
2023-11-10 13:08:32 +0530

    EditNoteIcon

--- src/view/pages/pages.js
+++ src/view/pages/pages.js
@@ -1,8 +1,9 @@
 import PublicIcon from "@mui/icons-material/Public";
-import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+
 import ListIcon from "@mui/icons-material/List";
+import EditNoteIcon from '@mui/icons-material/EditNote';
 export const PAGE_IDX = {
   config: { Icon: ListIcon },
   map: { Icon: PublicIcon },
-  data: { Icon: MenuOpenIcon },
+  data: { Icon: EditNoteIcon },
 };
