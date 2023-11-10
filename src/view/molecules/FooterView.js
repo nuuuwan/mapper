d80fc92df921ebdc50e4f50c10a51538b3d982fb
nuuuwan <nuuuwan@gmail.com>
2023-11-10 10:25:33 +0530

    Removed Click Bug

--- src/view/molecules/FooterView.js
+++ src/view/molecules/FooterView.js
@@ -1,17 +1,13 @@
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 
-import BugIcon from "@mui/icons-material/BugReport";
 
 import { PAGE_IDX } from "../pages/pages.js";
 
 export default function FooterView({ onClickPage, pageId: selectedPageId }) {
-  const onClickBug = function () {
-    window.open("https://github.com/nuuuwan/mapper/issues/new", "_blank");
-  };
 
   return (
     <BottomNavigation>
-      <BottomNavigationAction icon={<BugIcon />} onClick={onClickBug} />
+
       {Object.entries(PAGE_IDX).map(function ([pageId, d]) {
         const key = "button-" + pageId;
         const isSelected = pageId === selectedPageId;
